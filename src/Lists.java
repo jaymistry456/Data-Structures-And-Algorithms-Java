5306613dffea76861c9b691c8fa2fdf31e39f182
jaymistry456 <jay.mistry456@yahoo.com>
2025-12-10 07:57:28 -0400

    Learn ArrayList and SinglyLinkedList

---
+++ src/Lists.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class Lists {
+    public static void main(String[] args) {
+        List<Employee> employeeList = new ArrayList<>();
+        // List<Employee> employeeList = new Vector<>(); // thread-safe ArrayList
+
+
+        // adding at the end of the list
+        employeeList.add(new Employee(1, "John", "Doe"));
+        employeeList.add(new Employee(2, "Jane", "Jones"));
+        employeeList.add(new Employee(3, "Harry", "Potter"));
+        employeeList.forEach(employee -> System.out.println(employee));
+
+        // accessing by index
+        System.out.println(employeeList.get(2));
+
+        // checking empty
+        System.out.println(employeeList.isEmpty());
+
+        // accessing the size
+        System.out.println(employeeList.size());
+
+        // updating by index
+        employeeList.set(1, new Employee(2, "Hermione", "Granger"));
+        System.out.println(employeeList.get(1));
+
+        // adding at a particular index
+        employeeList.add(1, new Employee(1, "Ron", "Weasley"));
+        System.out.println(employeeList.get(1));
+
+        // converting to an array
+        Employee[] employeeArray = employeeList.toArray(new Employee[employeeList.size()]);
+        for(Employee employee: employeeArray) {
+            System.out.println(employee);
+        }
+
+        // checking whether an item exists in the list (equals method needs to be overridden)
+        System.out.println(employeeList.contains(new Employee(3, "Harry", "Potter")));
+
+        // accessing the index of an item
+        System.out.println(employeeList.indexOf(new Employee(3, "Harry", "Potter")));
+
+        // deleting an item
+        employeeList.remove(0); // could also pass an item, but index is faster
+        employeeList.forEach(employee -> System.out.println(employee));
+    }
+}
---
+++ src/SinglyLinkedListImpl.java
@@ -0,0 +1,93 @@
+
+class LinkedListNode {
+    private Employee employee;
+    private LinkedListNode next;
+
+    public LinkedListNode(Employee employee) {
+        this.employee = employee;
+        next = null;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public void setNext(LinkedListNode next) {
+        this.next = next;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public LinkedListNode getNext() {
+        return next;
+    }
+
+    public String toString() {
+        return employee.toString();
+    }
+}
+
+class SinglyLinkedList {
+    private LinkedListNode head;
+    private int size;
+
+    public SinglyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public void addToFront(Employee employee) {
+        LinkedListNode newNode = new LinkedListNode(employee);
+        newNode.setNext(head);
+        head = newNode;
+        size++;
+    }
+
+    public LinkedListNode removeFromFront() {
+        if(isEmpty()) {
+            return null;
+        }
+        LinkedListNode nodeToRemove = head;
+        head = head.getNext();
+        nodeToRemove.setNext(null);
+        size--;
+        return nodeToRemove;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public void printList() {
+        if(isEmpty()) {
+            return;
+        }
+        LinkedListNode curr = head;
+        System.out.print("HEAD -> ");
+        while(curr != null) {
+            System.out.print(curr);
+            System.out.print(" -> ");
+            curr = curr.getNext();
+        }
+        System.out.println(" null");
+    }
+}
+
+public class SinglyLinkedListImpl {
+    public static void main(String[] args) {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+        linkedList.addToFront(new Employee(1, "Harry", "Potter"));
+        linkedList.addToFront(new Employee(2, "Ron", "Weasly"));
+        linkedList.addToFront(new Employee(3, "Hermione", "Granger"));
+        linkedList.printList();
+        linkedList.removeFromFront();
+        linkedList.printList();
+        System.out.println(linkedList.getSize());
+    }
+}
